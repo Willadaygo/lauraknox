1f7c7d2032964be1373ed26d1f700be0043f6a1d
Will Russell <williamflrussell@gmail.com>
2015-07-16 20:15:49 +0100

    nearly done, needs tidying

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,56 +5,75 @@
 
 $( function() {
 
-    //Header
-    $(window).scroll(function () {
-      var distanceY = $(this).scrollTop(),
-          shrinkOn  = 100,
-          header    = $(".header");
+  var $gallery,
+      shrinkOn = 100,
+      header = $('.header'),
+      classSmaller = 'is-smaller';
 
-      if (distanceY > shrinkOn) {
-        header.addClass("is-smaller");
-      } else {
-        if (header.hasClass("is-smaller")) {
-          header.removeClass("is-smaller");
-        }
+  //Header
+  $(window).scroll(function () {
+    var distanceY = $(this).scrollTop();
+
+    if (distanceY > shrinkOn) {
+      header.addClass(classSmaller);
+    } else {
+      if (header.hasClass(classSmaller) && !$gallery.isActive() ) {
+        header.removeClass(classSmaller);
       }
-    });
-
-    //Masonary Layout
-    var $grid = $( '.gallery' ).masonry(
-    {
-        itemSelector: '.gallery__item',
-        columnWidth: '.gallery__item-sizer',
-        percentPosition: true,
-        gutter: 20,
-        transitionDuration: 0,
-        stamp: '.gallery__featured'
-    });
-
-    //Images Loaded before Masonary Layout
-    $grid.imagesLoaded().progress( function() {
-      $grid.masonry('layout');
-    });
-
-    //Gallery Plugin
-    $('.gallery').lightGallery({
-        mode: 'fade',
-        speed: 300,
-        selector: '.gallery__thumb',
-        // addClass: ''
-        preload: 5,
-        showThumbByDefault: false,
-        thumbnail: false
-        // animateThumb: false
-
-    }); 
-
-    // #Fastclick Mobile delay
-    // #Amended for IE 
-    if (window.addEventListener) {
-      window.addEventListener('load', function() {
-        new FastClick(document.body);
-      }, false);
     }
+  });
+
+  //Masonary Layout
+  var $grid = $( '.gallery' ).masonry(
+  {
+    itemSelector: '.gallery__item',
+    columnWidth: '.gallery__item-sizer',
+    percentPosition: true,
+    gutter: 20,
+    transitionDuration: 0,
+    stamp: '.gallery__featured'
+  });
+
+  //Images Loaded before Masonary Layout
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+  });
+
+  //Light Gallery Plugin
+  $gallery = $('.gallery').lightGallery({
+    mode: 'fade',
+    speed: 300,
+    selector: '.gallery__thumb',
+    preload: 2,
+    showThumbByDefault: false,
+    thumbnail: false,
+    onOpen: function() {
+      header.addClass(classSmaller);
+    },
+    onCloseAfter: function() {
+      var distanceY = $(window).scrollTop();
+
+      if (distanceY < shrinkOn) {
+        header.removeClass(classSmaller);
+      }
+    }
+
+  });
+
+  $('.js-scroll-top').on('click', function(e){
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0,
+      }, 700
+    );
+  });
+
+  // #Fastclick Mobile delay
+  // #Amended for IE 
+  if (window.addEventListener) {
+    window.addEventListener('load', function() {
+      new FastClick(document.body);
+    }, false);
+  }
     
 });
